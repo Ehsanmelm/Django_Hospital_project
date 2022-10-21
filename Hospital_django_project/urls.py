ccb2bf2e87265aedac6e1b8100a4c274d3cde6d9
ehsan <ehsanmelm82@gmail.com>
2022-10-21 16:55:43 +0330

    add style for home page

--- Hospital_django_project/urls.py
+++ Hospital_django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Django-Hospital', include("hotpital.urls")),
+    path('Django-Hospital', include("hospital.urls")),
 ]
